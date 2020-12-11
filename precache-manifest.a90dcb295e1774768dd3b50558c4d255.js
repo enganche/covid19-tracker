e7225ca6487e5d74a6abfa387553a53cbee5493f
ngtzzz <nguoivietnam250403@gmail.com>
2020-12-11 15:15:33 +0700

    Updates

---
+++ precache-manifest.a90dcb295e1774768dd3b50558c4d255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69383af901ccc787e7a74148d9bbc43c",
+    "url": "/covid19_tracker/index.html"
+  },
+  {
+    "revision": "28599d1281cfbf58cde7",
+    "url": "/covid19_tracker/static/css/main.21710466.chunk.css"
+  },
+  {
+    "revision": "ff177dee09d15dbc18fd",
+    "url": "/covid19_tracker/static/js/2.bcab41ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19_tracker/static/js/2.bcab41ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28599d1281cfbf58cde7",
+    "url": "/covid19_tracker/static/js/main.bfe0dbbb.chunk.js"
+  },
+  {
+    "revision": "2970a6838b3dcec5b02b",
+    "url": "/covid19_tracker/static/js/runtime-main.c99c64e0.js"
+  }
+]);
